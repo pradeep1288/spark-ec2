6281b29e331ecd4761b101719c32926bd4776917
Pradeep Nayak <pradeepu@netapp.com>
2016-03-10 15:39:51 -0800

    ammeding author name

--- spark_ec2.py
+++ spark_ec2.py
@@ -813,7 +813,7 @@ def setup_cluster(conn, master_nodes, slave_nodes, opts, deploy_ssh_key):
             ssh_write(slave_address, opts, ['tar', 'x'], dot_ssh_tar)
 
     modules = ['spark', 'ephemeral-hdfs', 'persistent-hdfs',
-               'mapreduce', 'spark-standalone', 'tachyon', 'nfs-hdfs']
+               'mapreduce', 'spark-standalone', 'nfs-hdfs']
 
     if opts.hadoop_major_version == "1":
         modules = list(filter(lambda x: x != "mapreduce", modules))
