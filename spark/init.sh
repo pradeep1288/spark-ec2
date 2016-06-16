d54ea14d8eb6d39d03179db63c9bb164594b3fe5
Pradeep Nayak <pradeepu@netapp.com>
2016-06-16 13:35:08 -0700

    spark 2.0 installation

--- spark/init.sh
+++ spark/init.sh
@@ -130,7 +130,7 @@ else
       elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
         wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-cdh4.tgz
       else
-        wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-hadoop2.4.tgz
+        wget http://apache.mirrors.pair.com/spark/spark-2.0.0-preview/spark-2.0.0-preview-bin-hadoop2.7.tgz
       fi
       if [ $? != 0 ]; then
         echo "ERROR: Unknown Spark version"
